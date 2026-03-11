639b74ff6083c694d269b5fd26520d5914b367e4
mahdi-misk <mahdimisk6@gmail.com>
2026-03-12 02:57:03 +0300

    Auto update by mahdi at 2026-03-12 02:57:03

---
+++ auto_push.py
@@ -0,0 +1,31 @@
+import os
+import subprocess
+import getpass
+from datetime import datetime
+import time
+
+repo_path = r"E:\my project\private\robo fight"
+
+os.chdir(repo_path)
+
+username = getpass.getuser()
+
+while True:
+
+    status = subprocess.check_output("git status --porcelain", shell=True).decode()
+
+    if status.strip() != "":
+
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        message = f"Auto update by {username} at {now}"
+
+        subprocess.run("git add .", shell=True)
+        subprocess.run(f'git commit -m "{message}"', shell=True)
+        subprocess.run("git push", shell=True)
+
+        print("Pushed:", message)
+
+    else:
+        print("No changes")
+
+    time.sleep(60)
